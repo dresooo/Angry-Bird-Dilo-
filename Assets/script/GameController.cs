f99eabcdeed0bc537fd4ae34ac076a4b67068e50
dresooo <andresmalvinj@gmail.com>
2021-05-03 16:22:39 +0700

    First Comit

---
+++ Assets/script/GameController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public SlingShooter SlingShooter;
+    public List<Bird> Birds;
+    public TrailController TrailController;
+    public List<Enemy> Enemies;
+    public BoxCollider2D TapCollider;
+    private bool _isGameEnded = false;
+    private Bird _shotBird;
+    void Start()
+    {
+        for (int i = 0; i < Birds.Count; i++)
+        {
+            Birds[i].OnBirdDestroyed += ChangeBird;
+            Birds[i].OnBirdShot += AssignTrail;
+        }
+        SlingShooter.InitiateBird(Birds[0]);
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            Enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
+        TapCollider.enabled = false;
+        SlingShooter.InitiateBird(Birds[0]);
+        _shotBird = Birds[0];
+    }
+    
+    void OnMouseUp()
+    {
+        if (_shotBird != null)
+        {
+            _shotBird.OnTap();
+        }
+    }
+    public void ChangeBird()
+    {
+        TapCollider.enabled = false;
+        if (_isGameEnded)
+        {
+            return;
+        }
+        Birds.RemoveAt(0);
+
+        if (Birds.Count > 0)
+        {
+            SlingShooter.InitiateBird(Birds[0]);
+            _shotBird = Birds[0];
+        }
+    }
+    public void AssignTrail(Bird bird)
+    {
+        TrailController.SetBird(bird);
+        StartCoroutine(TrailController.SpawnTrail());
+        TapCollider.enabled = true;
+    }
+    public void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            if (Enemies[i].gameObject == destroyedEnemy)
+            {
+                Enemies.RemoveAt(i);
+                break;
+            }
+        }
+
+        if (Enemies.Count == 0)
+        {
+            _isGameEnded = true;
+        }
+    }
+}
---
+++ Assets/script/Owl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Owl : Bird
+{
+    public float radius = 5.0f;
+    public float power = 1.0f;
+    public float torque = 3.0f;
+    public bool _hasBoost = false;
+    public GameObject Explotion_Object;
+
+    public override void OnTap()
+    {
+        explotion();
+    }
+
+    void explotion()
+    {
+        //Mengambil titik saat ledakan
+        Vector3 explotionPos = transform.position;
+        //Mengambil colider diarea titik ledakan dengan radius tertentu dalam bentu array
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(explotionPos, radius);
+
+        //Pengambilan 1 per satu colider yg didpt
+        foreach (Collider2D hit in colliders)
+        {
+            //Mengambil Rigidbody yang terdapat pada objek colider
+            Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
+
+            if (rb != null &&!_hasBoost)
+            {
+                //Memberikan dorongan dari titik ledakan dengan kekuatan yg ditentukan
+                rb.AddRelativeForce(explotionPos * power, ForceMode2D.Impulse);
+                //Memberikan putaran pada objek
+                rb.AddTorque(torque, ForceMode2D.Impulse);
+                _hasBoost = true;
+                Instantiate(Explotion_Object, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+
+
+        }
+    }
+}
